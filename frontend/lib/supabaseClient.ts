191e62fff8beb98cef67a4876476153231426a0d
Rabia Zulfiqar <rzulfiqar889@gmail.com>
2026-08-06 17:04:08 +0500

    add env vars

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =  "https://rjiphbklnvavzstkalqi.supabase.co";
-const supabaseAnonKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqaXBoYmtsbnZhdnpzdGthbHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NzM1MzEsImV4cCI6MjA4NTQ0OTUzMX0.JhSeMknmJOp8vH_aedWNwrpvJbdtzISltvtrHobEyIE";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
